1103ff10d47e8d67ac1a1787d2691cfa51119c72
ragin-hari <70678944+ragin-hari@users.noreply.github.com>
2024-03-23 19:09:22 -0300

    Commit Inicial

---
+++ game_stats.py
@@ -0,0 +1,14 @@
+class GameStats:
+    """ Tracks statistics for Alien Invasion."""
+    
+    def __init__(self, ai_game):
+        """ Initialize Statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        
+        #Start Alien invasion in an inactive state.
+        self.game_active = False
+        
+    def reset_stats(self):
+        """ Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
